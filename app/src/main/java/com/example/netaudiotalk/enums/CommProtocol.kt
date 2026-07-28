229f43cf721405b550e296d7c41722906511821a
hugozhutc <775801261@qq.com>
2026-07-28 11:55:32 +0800

    Delete app directory

--- app/src/main/java/com/example/netaudiotalk/enums/CommProtocol.kt
+++
@@ -1,7 +0,0 @@
-package com.example.netaudiotalk.enums
-
-enum class CommProtocol {
-    TCP,
-    UDP_UNICAST,
-    UDP_MULTICAST
-}
--- app/src/main/java/com/example/netaudiotalk/enums/TalkMode.kt
+++
@@ -1,6 +0,0 @@
-package com.example.netaudiotalk.enums
-
-enum class TalkMode {
-    PTT,          // 按住说话，松开停止
-    CONTINUOUS    // 持续发送模式
-}
